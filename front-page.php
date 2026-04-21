e0149638edef1da53c2c2fffd06a694e0772d7d3
zildevvn <vuongdevvn0509@gmail.com>
2026-04-21 21:57:37 +0700

    clear code

--- front-page.php
+++ front-page.php
@@ -7,15 +7,7 @@
 get_header();
 ?>
 <main id="primary" class="site-main template-home">
-    <?php get_template_part('template-parts/home/hero-section'); ?>
-    <?php get_template_part('template-parts/home/news-section'); ?>
-    <?php get_template_part('template-parts/home/about-section'); ?>
-    <?php get_template_part('template-parts/home/reason-section'); ?>
-    <?php get_template_part('template-parts/home/program-section'); ?>
-    <?php get_template_part('template-parts/home/staff-section'); ?>
-    <?php get_template_part('template-parts/home/record-section'); ?>
-    <?php get_template_part('template-parts/home/partner-section'); ?>
-    <?php get_template_part('template-parts/shared/contact-section'); ?>
-    </main>
+
+</main>
 <?php
 get_footer();
