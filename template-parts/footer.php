469f8e8ad80ffed571885a36c53d1a1ae8737428
zildevvn <vuongdevvn0509@gmail.com>
2026-04-22 23:04:56 +0700

    update style header and footer on mobile

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,86 +1,55 @@
 <?php
-$copyright = get_field('copyright', 'option');
-$rights_reserved = get_field('rights_reserved', 'option');
+$copyright = get_field('copyright_ft', 'option');
 $custom_logo_id = get_theme_mod('custom_logo');
 $logo_url = wp_get_attachment_url($custom_logo_id);
-$socials = get_field('socials', 'option');
-$company_name = get_field('company_name', 'option');
-$address = get_field('address', 'option');
+$company_name = get_field('company_name_ft', 'option');
+$address = get_field('address_ft', 'option');
 ?>
-
 
 <?php get_template_part('template-parts/shared/contact-section'); ?>
 
 <footer class="main-footer">
-    <button class="btn-top d-flex align-items-center justify-content-center" type="button"
-        aria-label="<?php echo __('Back to top', 'nmc'); ?>">
-        <div class="btn-top__icon">
-            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none"
-                xmlns="http://www.w3.org/2000/svg" color="#000000">
-                <path d="M6 15L12 9L18 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round"></path>
-            </svg>
-        </div>
+    <div class="main-footer-top">
+        <div class="container">
+            <div class="main-footer-top-warp">
+                <div class="main-footer-top__left">
+                    <div class="main-footer__logo">
+                        <a href="<?php echo home_url(); ?>" aria-label="<?php echo get_bloginfo('name'); ?>">
+                            <img src="<?php echo $logo_url; ?>" alt="<?php echo get_bloginfo('name'); ?>">
+                        </a>
+                    </div>
 
-        PAGETOP
-    </button>
+                    <?php if (!empty($company_name)): ?>
+                        <p class="main-footer__company mb-0">
+                            <span>会社名</span>
+                            <?php echo $company_name; ?>
+                        </p>
+                    <?php endif; ?>
 
-    <div class="container">
-        <div class="main-footer-inner d-flex justify-content-between flex-wrap flex-lg-nowrap">
-            <div class="main-footer-left order-2">
-                <div class="main-footer-logo">
-                    <a href="<?php echo home_url(); ?>" aria-label="<?php echo get_bloginfo('name'); ?>">
-                        <img src="<?php echo $logo_url; ?>" alt="<?php echo get_bloginfo('name'); ?>">
-                    </a>
+                    <?php if (!empty($address)): ?>
+                        <p class="main-footer__address mb-0">
+                            <span>住所</span>
+                            <?php echo $address; ?>
+                        </p>
+                    <?php endif; ?>
                 </div>
 
-                <?php if ($company_name): ?>
-                    <p class="main-footer__company mb-0"> <span>会社名</span> <?php echo $company_name; ?></p>
-                <?php endif; ?>
-
-                <?php if ($address): ?>
-                    <p class="main-footer__address mb-0"> <span>住所</span> <?php echo $address; ?></p>
+                <?php if (has_nav_menu('footer-menu')): ?>
+                    <div class="main-footer-top__right">
+                        <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'menu_class' => 'footer-menu')) ?>
+                    </div>
                 <?php endif; ?>
             </div>
+        </div>
+    </div>
 
-            <?php if (has_nav_menu('primary-menu')): ?>
-                <div class="main-footer-right order-1 order-md-2">
-                    <div class="main-footer-menu menu-primary">
-                        <?php wp_nav_menu(array('theme_location' => 'primary-menu', 'menu_class' => 'footer-menu')) ?>
-                    </div>
-                </div>
+    <div class="main-footer-bottom">
+        <div class="container">
+            <?php if (!empty($copyright)): ?>
+                <p class="main-footer__copyright mb-0">
+                    <?php echo $copyright; ?>
+                </p>
             <?php endif; ?>
         </div>
-
-        <?php if ($socials): ?>
-            <ul class="main-footer__socials sazukaru-socials d-flex align-items-center justify-content-center">
-                <?php foreach ($socials as $social): ?>
-                    <li>
-                        <a href="<?= $social['link'] ?>" target="_blank">
-                            <?php
-                            $icon_url = $social['icon'];
-                            $icon_ext = pathinfo(parse_url($icon_url, PHP_URL_PATH), PATHINFO_EXTENSION);
-                            if (strtolower($icon_ext) === 'svg') {
-                                $icon_path = str_replace(site_url('/'), ABSPATH, $icon_url);
-                                if (file_exists($icon_path)) {
-                                    echo file_get_contents($icon_path);
-                                } else {
-                                    echo file_get_contents($icon_url);
-                                }
-                            } else {
-                                ?>
-                                <img src="<?= $icon_url ?>" alt="<?= $social['title'] ?>">
-                            <?php } ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-
-        <?php if ($copyright): ?>
-            <p class="main-footer__copyright d-flex align-items-center justify-content-center mb-0">
-                <?php echo $copyright; ?>
-            </p>
-        <?php endif; ?>
     </div>
 </footer>
--- template-parts/shared/contact-section.php
+++ template-parts/shared/contact-section.php
@@ -1,3 +1,6 @@
+<?php
+$phone = get_field('phone_header', 'option');
+?>
 <section class="yutaka-section contact-section">
     <div class="yutaka-section__bg">
         <img class="d-none d-lg-block"
@@ -9,5 +12,31 @@
     <div class="container text-center">
         <p class="yutaka-section__sub-heading">contact</p>
         <h2 class="yutaka-section__heading mb-0">お問い合わせ</h2>
+        <div class="contact-section__line"> </div>
+        <div class="contact-section__description">
+            <p class="mb-0">会社売却・会社売買に関するご相談は無料です。</p>
+            <p class="mb-0">売却できるかどうか分からない段階でも、<br class="d-flex d-md-none" /> どうぞお気軽にお問い合わせください。</p>
+        </div>
+
+        <div class="contact-section__actions d-flex align-items-center justify-content-center flex-wrap flex-md-nowrap">
+            <a href="<?php echo home_url('/contact') ?>"
+                class="btn-contact d-flex align-items-center justify-content-center">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-btn-contact.png"
+                    alt="image contact" />
+                お問い合わせはこちら
+            </a>
+
+            <?php if (!empty($phone)): ?>
+                <a href="tel:<?php echo $phone; ?>" class="btn-phone d-flex align-items-center justify-content-center">
+                    <img src=" <?php echo get_template_directory_uri(); ?>/assets/images/icon-phone-dark.png"
+                        alt="image phone" />
+
+                    <p class="mb-0">
+                        <span>電話にてご相談</span>
+                        <?php echo $phone; ?>
+                    </p>
+                </a>
+            <?php endif; ?>
+        </div>
     </div>
 </section>
